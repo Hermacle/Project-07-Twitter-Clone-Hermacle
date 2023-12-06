4c5567b736cd9e84250b1c763431b8a897d9fcab
Hermacle <kihenyeghohermacle@gmail.com>
2023-12-06 23:34:31 +0100

    Add Profile Page

---
+++ src/pages/ProfilePage.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Header from '../components/Header/Header';
+import Avatar from '../components/Tweet-editor/Avatar';
+import TweetEditorForm from '../components/Tweet-editor/TweetEditorForm';
+import Trends from '../components/Trends/Trends';
+
+import Tweets from '../components/Tweets/Tweets';
+import Sidebar from '../components/Sidebar/Sidebar';
+import Profile from '../components/Profile/Profile';
+
+export default function ProfilePage() {
+  return (
+    <>
+    <Sidebar />
+    <main className="timeline">
+      <section >
+        <Profile />
+        <Tweets />
+      </section>
+    </main>
+    <Trends />
+    </>
+  );
+}

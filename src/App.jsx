c777f8d8d6b0d3401dcfce50773baf4006e20c41
Hermacle <kihenyeghohermacle@gmail.com>
2023-12-06 23:34:31 +0100

    Add Profile Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import Home from "./pages/home.jsx";
+import ProfilePage from "./pages/ProfilePage.jsx";
+
 import Layout from "./components/layout.jsx";
+import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom"
 
 import('./style/reset.css');
 import('./style/App.css');
@@ -7,7 +10,12 @@ import('./style/App.css');
 export default function App() {
   return (
     <Layout>
-      <Home />
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<ProfilePage />} />
+      </Routes>
+      </BrowserRouter>
     </Layout>
   );
 }
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,4 +1,4 @@
-
+import { NavLink } from "react-router-dom";
 
 function Sidebar() {
     return (
@@ -10,14 +10,14 @@ function Sidebar() {
 
                 <div>
                     <ul>
-                        <li><img src="src/Icons/Home-Fill.svg" alt="" />Home</li>
-                        <li><img src="src/Icons/Explore.svg" alt="" /> Explore</li>
-                        <li><img src="src/Icons/Notifications.svg" alt="" /> Notifications</li>
-                        <li><img src="src/Icons/Messages.svg" alt="" /> Messages</li>
-                        <li><img src="src/Icons/Bookmarks.svg" alt="" /> Bookmarks</li>
-                        <li><img src="src/Icons/Lists.svg" alt="" /> List</li>
-                        <li><img src="src/Icons/Profile.svg" alt="" /> Profile</li>
-                        <li><img src="src/Icons/More.svg" alt="" /> More</li>
+                        <li><NavLink to="/" className="navigation"><img src="src/Icons/Home-Fill.svg" alt="" />Home</NavLink> </li>
+                        <li><NavLink to="/explore" className="navigation"><img src="src/Icons/Explore.svg" alt="" /> Explore</NavLink> </li>
+                        <li><NavLink to="/notifications" className="navigation"><img src="src/Icons/Notifications.svg" alt="" /> Notifications</NavLink></li>
+                        <li><NavLink to="/messages" className="navigation"><img src="src/Icons/Messages.svg" alt="" /> Messages</NavLink> </li>
+                        <li><NavLink to="/messages" className="navigation"><img src="src/Icons/Bookmarks.svg" alt="" /> Bookmarks</NavLink></li>
+                        <li><NavLink to="list" className="navigation"><img src="src/Icons/Lists.svg" alt="" /> List</NavLink></li>
+                        <li><NavLink to="/profile" className="navigation"><img src="src/Icons/Profile.svg" alt="" /> Profile</NavLink></li>
+                        <li><NavLink to="/more" className="navigation"><img src="src/Icons/More.svg" alt="" /> More</NavLink></li>
                     </ul>
                 </div>
                 <button className="button post-button">Tweet</button>

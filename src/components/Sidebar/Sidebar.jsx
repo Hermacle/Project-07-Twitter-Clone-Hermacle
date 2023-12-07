c0529f0be6f1c0c9011f013c310b6a8adeeb0053
Hermacle <kihenyeghohermacle@gmail.com>
2023-12-07 20:25:16 +0100

    Twitter Clone : v01

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -10,14 +10,12 @@ function Sidebar({homeIcon, profileIcon}) {
 
                 <div>
                     <ul>
-                        {/* <li><NavLink to="/" className="navigation"><img src="src/Icons/Home-Fill.svg" alt="" />Home</NavLink> </li> */}
                         <li><NavLink to="/" className="navigation"><img src={homeIcon} alt="" />Home</NavLink> </li>
                         <li><NavLink to="/explore" className="navigation"><img src="src/Icons/Explore.svg" alt="" /> Explore</NavLink> </li>
                         <li><NavLink to="/notifications" className="navigation"><img src="src/Icons/Notifications.svg" alt="" /> Notifications</NavLink></li>
                         <li><NavLink to="/messages" className="navigation"><img src="src/Icons/Messages.svg" alt="" /> Messages</NavLink> </li>
                         <li><NavLink to="/bookmarks" className="navigation"><img src="src/Icons/Bookmarks.svg" alt="" /> Bookmarks</NavLink></li>
                         <li><NavLink to="list" className="navigation"><img src="src/Icons/Lists.svg" alt="" /> List</NavLink></li>
-                        {/* <li><NavLink to="/profile" className="navigation"><img src="src/Icons/Profile.svg" alt="" /> Profile</NavLink></li> */}
                         <li><NavLink to="/profile" className="navigation"><img src={profileIcon} alt="" /> Profile</NavLink></li>
                         <li><NavLink to="/more" className="navigation"><img src="src/Icons/More.svg" alt="" /> More</NavLink></li>
                     </ul>
@@ -26,10 +24,10 @@ function Sidebar({homeIcon, profileIcon}) {
             </div>
             
             <div className="user-profil">
-                <img src="src/images/profile-photo.png" className="user-avatar" alt="icone" />
+                <img src="src/images/Profil-image_400x400.jpg" className="user-avatar" alt="icone" />
                 <div className="user-detail">
                     <h4>Hermacle <img src="src/Icons/Private.svg" alt="icone" /></h4>
-                    <p className="small-text">@Username</p>
+                    <p className="small-text">@Hermacle_kih</p>
                 </div>
                 <img src="src/Icons/More-2.svg" className="more-2" alt="icone" />
             </div>
--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,7 +1,4 @@
 import React from 'react';
-import Header from '../components/Header/Header';
-import Avatar from '../components/Tweet-editor/Avatar';
-import TweetEditorForm from '../components/Tweet-editor/TweetEditorForm';
 import Trends from '../components/Trends/Trends';
 
 import Tweets from '../components/Tweets/Tweets';

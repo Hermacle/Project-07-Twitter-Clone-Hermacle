daa7d37b1842b88a0e99b6948a4c61cd651d1ec8
Hermacle <kihenyeghohermacle@gmail.com>
2024-01-10 10:00:41 +0100

    refactor : tweet-editor

--- src/components/Tweet-editor/Avatar.jsx
+++ src/components/Tweet-editor/Avatar.jsx
@@ -1,9 +1,9 @@
 import avatarImage from './../../images/Profil-image_400x400.jpg'
 
 export default function Avatar() {
-    return (
-        <div className="avatar">
-            <img src={avatarImage}  alt="image" />
-        </div>
-    )
+  return (
+    <div className="avatar">
+      <img src={avatarImage} alt="image" />
+    </div>
+  )
 }
--- src/components/Tweet-editor/TweetEditorForm.jsx
+++ src/components/Tweet-editor/TweetEditorForm.jsx
@@ -1,35 +1,35 @@
- import smileIcon from './../../Icons/Emoji.svg';
+import smileIcon from './../../Icons/Emoji.svg';
 import gifIcon from './../../Icons/Gif.svg';
 import mediaIcon from './../../Icons/Media.svg';
 import pollIcon from './../../Icons/Poll.svg';
 import scheduleIcon from './../../Icons/Schedule.svg';
 
 function TweetEditorInput() {
-    return (
-        <input className="tweet-editor-input" placeholder="What's happening?"></input>
-    )
+  return (
+    <input className="tweet-editor-input" placeholder="What's happening?"></input>
+  )
 }
 function TweetEditorButtons() {
-    const buttonValue = 'Tweet';
-    return (
-        <div className="tweet-editor-buttons">
-            <div className="tweet-editor-actions"> 
-                <img src={mediaIcon} />
-                <img src={gifIcon} />
-                <img src={pollIcon} />
-                <img src={smileIcon} />
-                <img src={scheduleIcon} />
-            </div>
-            <button className="button">{buttonValue}</button>
-        </div>
-    )
+  const buttonValue = 'Tweet';
+  return (
+    <div className="tweet-editor-buttons">
+      <div className="tweet-editor-actions">
+        <img src={mediaIcon} />
+        <img src={gifIcon} />
+        <img src={pollIcon} />
+        <img src={smileIcon} />
+        <img src={scheduleIcon} />
+      </div>
+      <button className="button">{buttonValue}</button>
+    </div>
+  )
 }
 
 export default function TweetEditorForm() {
-    return (
-        <div className="tweet-editor-form">
-            <TweetEditorInput />
-            <TweetEditorButtons />
-        </div>
-    )
+  return (
+    <div className="tweet-editor-form">
+      <TweetEditorInput />
+      <TweetEditorButtons />
+    </div>
+  )
 }

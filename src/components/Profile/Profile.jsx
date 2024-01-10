26e22d976e7dd06ae8a0ed8b282bab7c5d9a3e26
Hermacle <kihenyeghohermacle@gmail.com>
2024-01-10 11:55:15 +0100

    feat : add action icons to profile

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -4,6 +4,9 @@ import ImageUser from "./ImageUser";
 import ProfileNavigation from "./ProfileNavigation";
 import ButtonAction from "./ButtonAction";
 import AboutUser from "./AboutUser";
+ 
+const moreIcon = <img src="src/Icons/More-2.svg" alt="More Icon" />
+const msgIcon = <img src="src/Icons/Messages.svg" alt="Message Icon" />
 
 export default function Profile() {
   return (
@@ -13,8 +16,8 @@ export default function Profile() {
       <section className="container">
         <div className="about-user">
           <div className="button-action">
-            <ButtonAction value="..." className="icon" />
-            <ButtonAction value="Ms" className="icon" />
+            <ButtonAction value={moreIcon} className="icon" />
+            <ButtonAction value={msgIcon} className="icon" />
             <ButtonAction value="Follow" className="button-follow" />
             <ButtonAction value="Edit profile" className="button-edit" />
           </div>

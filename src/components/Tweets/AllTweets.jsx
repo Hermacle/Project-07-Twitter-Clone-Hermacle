07190ed298e95b3873345a5233e2c40b62b942de
Hermacle <kihenyeghohermacle@gmail.com>
2024-01-11 20:06:42 +0100

    refactor : All Tweets

--- src/components/Tweets/AllTweets.jsx
+++ src/components/Tweets/AllTweets.jsx
@@ -1,18 +1,22 @@
 import React from 'react';
 import "./Tweets.css";   
- 
 import data from '../Data/data';
 import TweetModel from './TweetModel';
  
 function AllTweets({ icon }) {
+  // Parcourir l'objet de données pour obtenir les tweets de tous les utilisateurs
+  const allTweets = Object.keys(data).map((user) => {
+
+    const userData = data[user];
+    // Parcourir les tweets de chaque utilisateur et créer un composant TweetModel pour chaque tweet
+    return userData.tweets.map((tweet) => (
+      <TweetModel key={tweet.id} tweet={tweet} userData={userData.profile} icon={icon} />
+    ));
+  });
+
   return (
     <div>
-      {Object.keys(data).map((user) => {
-        const userData = data[user];
-        return userData.tweets.map((tweet) => (
-          <TweetModel key={tweet.id} tweet={tweet} userData={userData.profile} icon={icon} />
-        ));
-      })}
+      {allTweets}
     </div>
   );
 } 

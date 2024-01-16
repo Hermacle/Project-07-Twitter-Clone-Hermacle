7047419945887beefa99a8f701d9f19001b0fd2b
Hermacle <kihenyeghohermacle@gmail.com>
2024-01-16 21:26:07 +0100

    fix : redirect links

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -12,13 +12,13 @@ function Sidebar({ homeIcon, profileIcon }) {
         <div>
           <ul>
             <li><NavLink to={appRoutes.home} className="navigation"><img src={homeIcon} alt="" />Home</NavLink> </li>
-            <li><NavLink to="/explore" className="navigation"><img src="src/Icons/Explore.svg" alt="" /> Explore</NavLink> </li>
-            <li><NavLink to="/notifications" className="navigation"><img src="src/Icons/Notifications.svg" alt="" /> Notifications</NavLink></li>
-            <li><NavLink to="/messages" className="navigation"><img src="src/Icons/Messages.svg" alt="" /> Messages</NavLink> </li>
-            <li><NavLink to="/bookmarks" className="navigation"><img src="src/Icons/Bookmarks.svg" alt="" /> Bookmarks</NavLink></li>
-            <li><NavLink to="list" className="navigation"><img src="src/Icons/Lists.svg" alt="" /> List</NavLink></li>
+            <li><NavLink to="#" className="navigation"><img src="src/Icons/Explore.svg" alt="" /> Explore</NavLink> </li>
+            <li><NavLink to="#" className="navigation"><img src="src/Icons/Notifications.svg" alt="" /> Notifications</NavLink></li>
+            <li><NavLink to="#" className="navigation"><img src="src/Icons/Messages.svg" alt="" /> Messages</NavLink> </li>
+            <li><NavLink to="#" className="navigation"><img src="src/Icons/Bookmarks.svg" alt="" /> Bookmarks</NavLink></li>
+            <li><NavLink to="#" className="navigation"><img src="src/Icons/Lists.svg" alt="" /> List</NavLink></li>
             <li><NavLink to={appRoutes.profile} className="navigation"><img src={profileIcon} alt="" /> Profile</NavLink></li>
-            <li><NavLink to="/more" className="navigation"><img src="src/Icons/More.svg" alt="" /> More</NavLink></li>
+            <li><NavLink to="#" className="navigation"><img src="src/Icons/More.svg" alt="" /> More</NavLink></li>
           </ul>
         </div>
         <button className="button post-button">Tweet</button>

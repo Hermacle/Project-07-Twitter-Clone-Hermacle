19ecf6a947aa6783ec20dbfbe163fb1a3a8f488b
Hermacle <kihenyeghohermacle@gmail.com>
2024-01-17 16:03:47 +0100

    refactor : right sidebar components

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
+
+//left sidebar
+import Sidebar from '../components/Sidebar/Sidebar';
+
+// tweet components
 import Header from '../components/Header/Header';
 import Avatar from '../components/Tweet-editor/Avatar';
 import TweetEditorForm from '../components/Tweet-editor/TweetEditorForm';
-import Trends from '../components/Trends/Trends';
-
-import Sidebar from '../components/Sidebar/Sidebar';
 import AllTweets from '../components/Tweets/AllTweets';
+
+//the components of the right sidebar
+import Search from '../components/Trends/Search';
+import TrendsCard from '../components/Trends/TrendsCard';
+import FollowCard from '../components/Trends/FollowCard';
+import PolicyTerms from '../components/Trends/PolicyTerms';
 
 // React Icons 
 import { HiCheckBadge } from 'react-icons/hi2';
@@ -24,7 +32,13 @@ export default function Home() {
           <AllTweets icon={<HiCheckBadge />} />
         </section>
       </main>
-      <Trends />
+      <div className="right-sidebar">
+        <Search />
+        <TrendsCard />
+        <FollowCard />
+        <PolicyTerms />
+      </div>
+
     </>
   );
 }

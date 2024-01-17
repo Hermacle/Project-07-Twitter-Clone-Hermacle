868414bf7b02c108a1e004da0d1222f625d5ad91
Hermacle <kihenyeghohermacle@gmail.com>
2024-01-17 15:14:36 +0100

    refactor : creation of the search component separately

---
+++ src/components/Trends/Search.jsx
@@ -0,0 +1,9 @@
+function Search() {
+  return (
+    <div className="search">
+      <img src="src/Icons/Search.svg" alt="Search Icon" />
+      <input type="search" name="" id="" placeholder="Search Twitter" />
+    </div>
+  )
+}
+export default Search;

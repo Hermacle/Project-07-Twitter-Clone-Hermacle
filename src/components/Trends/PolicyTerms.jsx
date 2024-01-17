0a7410246523b27efe0a587395739186dd90dcb4
Hermacle <kihenyeghohermacle@gmail.com>
2024-01-17 15:47:58 +0100

    refactor : creation of the PolicyTerms component separately

---
+++ src/components/Trends/PolicyTerms.jsx
@@ -0,0 +1,15 @@
+
+function PolicyTerms() {
+  return (
+    <div className="policy">
+      <a href="#">Terms of Service</a>
+      <a href="#">Privacy Policy</a>
+      <a href="#">Cookie Policy</a> <br /> <br />
+      <a href="#">Imprint</a>
+      <a href="#">Ads info</a>
+      <a href="#">More ...</a>
+      <span> © 2021 Twiter, Inc.</span>
+    </div>
+  )
+}
+export default PolicyTerms
--- src/components/Trends/Trends.jsx
+++ src/components/Trends/Trends.jsx
@@ -1,27 +1,16 @@
 import Search from "./Search";
 import TrendsCard from "./TrendsCard";
 import FollowCard from "./FollowCard";
+import PolicyTerms from "./PolicyTerms";
 
-function Policy() {
-  return (
-    <div className="policy">
-      <a href="#">Terms of Service</a>
-      <a href="#">Privacy Policy</a>
-      <a href="#">Cookie Policy</a> <br /> <br />
-      <a href="#">Imprint</a>
-      <a href="#">Ads info</a>
-      <a href="#">More ...</a>
-      <span> © 2021 Twiter, Inc.</span>
-    </div>
-  )
-}
+
 export default function Trends() {
   return (
     <div className="right-sidebar">
       <Search />
       <TrendsCard />
       <FollowCard />
-      <Policy />
+      <PolicyTerms />
     </div>
   )
 }

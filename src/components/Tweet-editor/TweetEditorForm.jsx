3172cfaf09b45cef5948703b2203f9a13d1af1f2
Hermacle <kihenyeghohermacle@gmail.com>
2024-01-18 10:33:18 +0100

    feat : adding tweets to data via input

--- src/components/Tweet-editor/TweetEditorForm.jsx
+++ src/components/Tweet-editor/TweetEditorForm.jsx
@@ -1,35 +1,58 @@
+import React, { useState } from 'react';
 import smileIcon from './../../Icons/Emoji.svg';
 import gifIcon from './../../Icons/Gif.svg';
 import mediaIcon from './../../Icons/Media.svg';
 import pollIcon from './../../Icons/Poll.svg';
 import scheduleIcon from './../../Icons/Schedule.svg';
+import data from '../Data/data';
 
-function TweetEditorInput() {
+function TweetEditorInput({ newTweetText, setNewTweetText }) {
   return (
-    <input className="tweet-editor-input" placeholder="What's happening?"></input>
-  )
+    <input className="tweet-editor-input" placeholder="What's happening?" value={newTweetText}
+      onChange={(e) => setNewTweetText(e.target.value)} />
+  );
 }
-function TweetEditorButtons() {
+
+function TweetEditorButtons({ handleTweetButtonClick }) {
   const buttonValue = 'Tweet';
   return (
     <div className="tweet-editor-buttons">
       <div className="tweet-editor-actions">
-        <img src={mediaIcon} />
-        <img src={gifIcon} />
-        <img src={pollIcon} />
-        <img src={smileIcon} />
-        <img src={scheduleIcon} />
+        <img src={mediaIcon} alt="Media Icon" />
+        <img src={gifIcon} alt="GIF Icon" />
+        <img src={pollIcon} alt="Poll Icon" />
+        <img src={smileIcon} alt="Smile Icon" />
+        <img src={scheduleIcon} alt="Schedule Icon" />
       </div>
-      <button className="button">{buttonValue}</button>
+      <button className="button" onClick={handleTweetButtonClick}>
+        {buttonValue}
+      </button>
     </div>
-  )
+  );
 }
 
 export default function TweetEditorForm() {
+  const [newTweetText, setNewTweetText] = useState('');
+
+  function handleTweetButtonClick(){
+    const newTweet = {
+      id: "Tweet2_ID",
+      text: newTweetText,
+      imagePost: "",
+      timePost: "now",  
+      commentCount: 0,
+      likeCount: 0,
+      repostCount: 0,
+      shareCount: 0
+    };
+    data.Hermacle.tweets.unshift(newTweet);
+    setNewTweetText('');  
+  };
+
   return (
     <div className="tweet-editor-form">
-      <TweetEditorInput />
-      <TweetEditorButtons />
+      <TweetEditorInput newTweetText={newTweetText} setNewTweetText={setNewTweetText} />
+      <TweetEditorButtons handleTweetButtonClick={handleTweetButtonClick} />
     </div>
-  )
+  );
 }

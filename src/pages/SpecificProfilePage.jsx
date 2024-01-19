0d4a896593a361c812a76c68b2e492120f18719c
Hermacle <kihenyeghohermacle@gmail.com>
2024-01-19 09:34:49 +0100

    refactor : creating a global useparams to specify a profile and its tweets

--- src/pages/SpecificProfilePage.jsx
+++ src/pages/SpecificProfilePage.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { useParams } from "react-router-dom";
+import data from '../components/Data/data';
 
 //left sidebar
 import Sidebar from '../components/Sidebar/Sidebar';
@@ -13,16 +15,36 @@ import TrendsCard from '../components/Trends/TrendsCard';
 import FollowCard from '../components/Trends/FollowCard';
 import PolicyTerms from '../components/Trends/PolicyTerms';
 
+
 // React Icons 
 import { HiCheckBadge } from 'react-icons/hi2';
 
 export default function SpecificProfilePage({ newProfile }) {
+
+
+  const { id } = useParams();
+  const clickedUser = Object.values(data).find(user => user.profile.author === id);
+
+  if (!clickedUser) {
+    return <p>Le PROFIL que vous avez demandé n'existe pas.</p>;
+  }
+
   return (
     <>
       <Sidebar homeIcon="src/Icons/Home.svg" profileIcon="src/Icons/Profile.svg" />
       <main className="timeline">
         <section >
-          <SpecificProfile />
+          <SpecificProfile
+            author={clickedUser.profile.author}
+            posts={clickedUser.tweets.length}
+            userName={clickedUser.profile.userName}
+            avatar={clickedUser.profile.avatar}
+            imageCover={clickedUser.profile.imageCover}
+            description={clickedUser.profile.description}
+            tags={clickedUser.profile.tags}
+            followers={clickedUser.profile.followers}
+            followings={clickedUser.profile.followings}
+          />
           <SpecificTweets icon={<HiCheckBadge />} />
         </section>
       </main>
